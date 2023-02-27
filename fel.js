abf0ae9373020bb4f425073f1f2a420ce2c54219
KissBendeguz1 <113110759+KissBendeguz1@users.noreply.github.com>
2023-02-27 19:28:29 +0100

    Delete fel.js

--- fel.js
+++
@@ -1,6 +0,0 @@
-let mybutton = document.getElementById("myBtn");
-
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-  }
